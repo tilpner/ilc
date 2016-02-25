cd15d4f8de1ee9bc6d00fb0a08659ca2c74d2e2d
Till Höppner <till@hoeppner.ws>
2016-02-25 06:58:22 +0100

    Update Cargo.toml files with more metadata

--- src/context.rs
+++
@@ -1,9 +0,0 @@
-
-use chrono::naive::date::NaiveDate;
-use chrono::offset::fixed::FixedOffset;
-
-pub struct Context {
-    pub timezone: FixedOffset,
-    pub override_date: Option<NaiveDate>,
-    pub channel: Option<String>,
-}
